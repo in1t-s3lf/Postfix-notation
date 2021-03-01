37740ef1500deb4d1ebc1f42cdc394fef72d121c
in1t-s3lf <36043458+in1t-s3lf@users.noreply.github.com>
2021-03-01 07:23:14 +0300

    Create postfix.py

---
+++ postfix.py
@@ -0,0 +1,24 @@
+
+def postfixEval(postfixExpr):
+    operandStack = []
+    tokenList = postfixExpr.split()
+
+    for token in tokenList:
+        if token in "0123456789":
+            operandStack.append(int(token))
+        else:
+            operand2 = operandStack.pop()
+            operand1 = operandStack.pop()
+            result = doMath(token,operand1,operand2)
+            operandStack.append(result)
+    return operandStack.pop()
+
+def doMath(op, op1, op2):
+    if op == "*":
+        return op1 * op2
+    elif op == "+":
+        return op1 + op2
+    else:
+        return op1 - op2
+expr = input()
+print(postfixEval(expr))
